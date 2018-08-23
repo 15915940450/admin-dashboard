d8acb16c5678373dd25ed31e8ab2ad01591045e0
15915940450 <15915940450@139.com>
2018-08-23 16:41:08 +0800

    service for what?

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes=HEROES;
+  heroes:Hero[];
 
   // hero:Hero={
   //   id:1,
@@ -17,13 +17,18 @@ export class HeroesComponent implements OnInit {
   selectedHero:Hero;
 
   
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero:Hero):void{
     this.selectedHero=hero;
   };
+
+  getHeroes():vold{
+    this.heroes=this.heroService.getHeroes();
+  }
 
 }
