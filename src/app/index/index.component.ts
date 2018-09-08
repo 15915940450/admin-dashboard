e35e16f8f81de1ae538536f4eafdfb402e985d5d
15915940450 <15915940450@139.com>
2018-09-08 14:48:35 +0800

    animations in ng

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostBinding } from '@angular/core';
+import {slideInDownAnimation} from '../animations';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  animations: [ slideInDownAnimation ]
 })
 export class IndexComponent implements OnInit {
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'absolute';
 
   constructor() { }
 
